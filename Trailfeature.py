8fec2c9b38512dfa7e999067716b2d814afb9bd2
Harvey Moth <Harvey.moth@students.plymouth.ac.uk>
2024-12-31 16:03:11 +0000

    Added some comments
    Added some comments to explain my thinking for future development

--- Trailfeature.py
+++ Trailfeature.py
@@ -11,7 +11,7 @@ def getall():
 
 
 def getone(id):
-    trailfeature = TrailFeature.query.filter(TrailFeature.TrailFeatureID == id).one_or_none()
+    trailfeature = TrailFeature.query.filter(TrailFeature.TrailFeatureID == id).one_or_none() #Not really needed as is, would be better to check and get all the features attached to a given trail ID 
     if trailfeature is not None:
         return TFschema.dump(trailfeature)
     else:
@@ -24,25 +24,22 @@ def create(trailfeature):
     db.session.commit()
     return TFschema.dump(newtrailfeature)
 
-def update(id, trailfeature):
-    updatetrailfeature = TrailFeature.query.filter(TrailFeature.TrailFeatureID == id).one_or_none()
-    if updatetrailfeature is not None:
-        update = TFschema.load(trailfeature, session=db.session)
-        update.TrailFeatureID = updatetrailfeature.TrailFeatureID 
-        db.session.merge(update)
-        db.session.commit()
-        return TFschema.dump(updatetrailfeature)
-    else:
-        abort(404, f"TrailFeature not found for Id: {id}")
+# def update(id, trailfeature):
+#     updatetrailfeature = TrailFeature.query.filter(TrailFeature.TrailFeatureID == id).one_or_none() 
+#     if updatetrailfeature is not None:
+#         update = TFschema.load(trailfeature, session=db.session)
+#         update.TrailFeatureID = updatetrailfeature.TrailFeatureID 
+#         db.session.merge(update)
+#         db.session.commit()
+#         return TFschema.dump(updatetrailfeature)
+#     else:
+#         abort(404, f"TrailFeature not found for Id: {id}")
 
 def delete(id): 
     trailfeature = TrailFeature.query.filter(TrailFeature.TrailFeatureID == id).one_or_none()
-            
-
-
     if trailfeature is not None:
         db.session.delete(trailfeature)
         db.session.commit()
         return make_response(f"TrailFeature {id} deleted", 200)
     else:
-        abort(404, f"TrailFeature not found for Id: {id}")
+        abort(404, f"TrailFeature not found for Id: {id}") # This is not going to work, as is.
